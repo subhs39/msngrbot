b1092c8e48b2280fd66e428be059d0d73e9322d9
subhs <subhammahato39@hotmail.com>
2017-11-25 19:53:30 +0530

    1st build

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def verify():
+    # Once the endpoint is added as a webhook, it must return back
+    # the 'hub.challenge' value it receives in the request arguments
+    if (request.args.get('hub.verify_token', '') == VERIFY_TOKEN):
+        print("Verified")
+        return request.args.get('hub.challenge', '')
+    else:
+        print('wrong verification token')
+        return "Error, Verification Failed"
