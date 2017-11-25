9714189522471afdf9dc381af661aed505ed2ed4
subhs <subhammahato39@hotmail.com>
2017-11-26 02:30:34 +0530

    first leap

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 
+PAGE_ACCESS_TOKEN = 'EAAJZCH7lRa4EBAHnaFLPFp83cXZCOO84ZB47FxtGtHHbkFxDMb025Qrxpbny7JS06UmGygqcmwpbvVjadRZCDqwZAMQV5ZCXOYeXcwa9tuXGIbtky2992RjJckScMx0xlQOPRBJjGdqy87LLve4HWzngN6PNsPFfDZBCSDQD3REzwTF5cyJLen3'
+VERIFY_TOKEN = 'messi'
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -12,3 +15,22 @@ def verify():
     else:
         print('wrong verification token')
         return "Error, Verification Failed"
+
+
+@app.route('/', methods=['POST'])
+def handle_message():
+    '''
+    Handle messages sent by facebook messenger to the applicaiton
+    '''
+    data = request.get_json()
+
+    log(data)
+    return "ok"
+
+def log(message):
+	print(message)
+	sys.stdout.flush()
+
+
+if __name__=="__main__":
+	app.run(debug=True, port= 80)
